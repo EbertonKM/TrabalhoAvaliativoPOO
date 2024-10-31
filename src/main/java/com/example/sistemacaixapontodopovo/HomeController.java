2262728ca31c83f2efbd421aeda4289261bd38fc
EbertonKM <144098194+EbertonKM@users.noreply.github.com>
2024-10-30 22:24:28 -0300

    Tentativa final de inserir imagem
    Imagem da tela home

--- src/main/java/com/example/sistemacaixapontodopovo/HomeController.java
+++ src/main/java/com/example/sistemacaixapontodopovo/HomeController.java
@@ -2,13 +2,19 @@ package com.example.sistemacaixapontodopovo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Objects;
 
 public class HomeController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ImageView imgView;
+
+    @FXML
+    protected void img(){
+        Image image = new Image("img/pontoDoPovoLogo-GeradaPorIA.jpg");
+        imgView.setImage(image);
     }
 }
