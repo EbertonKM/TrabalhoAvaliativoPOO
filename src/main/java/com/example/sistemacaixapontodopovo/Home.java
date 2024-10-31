ce775aa69daaae1505efb00c4589789f1583e75e
EbertonKM <ebertonkociba@gmail.com>
2024-10-31 05:03:46 -0300

    Front-End da página home (1.0)
    Concluido o front-end da página home via FXML e CSS

--- src/main/java/com/example/sistemacaixapontodopovo/Home.java
+++ src/main/java/com/example/sistemacaixapontodopovo/Home.java
@@ -11,7 +11,8 @@ public class Home extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Home.class.getResource("home-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+        Scene scene = new Scene(fxmlLoader.load(), 580, 275);
+        scene.getStylesheets().add(getClass().getResource("/style/style.css").toExternalForm());
         stage.setTitle("Ponto do Povo");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/sistemacaixapontodopovo/HomeController.java
+++ src/main/java/com/example/sistemacaixapontodopovo/HomeController.java
@@ -2,19 +2,24 @@ package com.example.sistemacaixapontodopovo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
 
 import java.util.Objects;
 
 public class HomeController {
 
     @FXML
-    private ImageView imgView;
+    private MenuItem menuItem1;
+    @FXML
+    private MenuItem menuItem2;
 
     @FXML
-    protected void img(){
-        Image image = new Image("img/pontoDoPovoLogo-GeradaPorIA.jpg");
-        imgView.setImage(image);
+    public void initialize() {
+        menuItem1.setAccelerator(new KeyCodeCombination(KeyCode.F1));
+        menuItem2.setAccelerator(new KeyCodeCombination(KeyCode.F2));
     }
 }
