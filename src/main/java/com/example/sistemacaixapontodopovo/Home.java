4c41eacfa8c60a2bc63fc85c97511e64058913f3
EbertonKM <ebertonkociba@gmail.com>
2024-10-31 18:13:15 -0300

    Inserido icones das janelas

--- src/main/java/com/example/sistemacaixapontodopovo/Home.java
+++ src/main/java/com/example/sistemacaixapontodopovo/Home.java
@@ -3,17 +3,20 @@ package com.example.sistemacaixapontodopovo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Home extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Home.class.getResource("home-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 580, 275);
-        scene.getStylesheets().add(getClass().getResource("/style/style.css").toExternalForm());
-        stage.setTitle("Ponto do Povo");
+        Scene scene = new Scene(fxmlLoader.load(), 580, 265);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/style/style.css")).toExternalForm());
+        stage.setTitle("Supermercado Ponto do Povo");
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/img/home-icon.png"))));
         stage.setScene(scene);
         stage.show();
     }
